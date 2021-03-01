912b3f64a7db8d580dd5ecab2ab081d9484122b7
Danielle Cruz <dcruz21@stanford.edu>
2021-03-01 16:46:29 -0600

    Added some example elements for our template, working on Stack Navigator library

--- altogether/App.js
+++ altogether/App.js
@@ -1,22 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Alert,
+  Button,
+  AppRegistry,
+  Image,
+  Keyboard,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { styles } from './Styles.js';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
+
+      {/* Example text */}
       <Text>Open up App.js to start working on your app!</Text>
       <Text>If you're seeing this, it means that you set up ALTogether correctly!</Text>
       <StatusBar style="auto" />
+
+      {/* Example button */}
+      <Text style={{marginTop: 20}}>Example button</Text>
+      <TouchableOpacity
+        style={styles.exampleButton}
+        onPress={() => Alert.alert('You just pressed an example button')}>
+        <Text style={{color: 'white', fontWeight: 'bold'}}>Example button</Text>
+      </TouchableOpacity>
+
+      {/* Example image */}
+      <Text style={{marginTop: 20}}>Example image</Text>
+      <Image
+        source = {require('./assets/images/ice_cream.png')}
+        style = {{width: 100, height: 100}}/>
     </View>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ altogether/InProgress1.js
@@ -0,0 +1,49 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import {
+  Alert,
+  Button,
+  AppRegistry,
+  Image,
+  Keyboard,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { styles } from './Styles.js';
+
+/* This page is in progress! Was trying to test something here */
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+      {/* Example text */}
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>If you're seeing this, it means that you set up ALTogether correctly!</Text>
+      <StatusBar style="auto" />
+
+      {/* Example button */}
+      <Text style={{marginTop: 20}}>Example button</Text>
+      <TouchableOpacity
+        style={styles.exampleButton}
+        onPress={() => Alert.alert('You just pressed an example button')}>
+        <Text style={{color: 'white', fontWeight: 'bold'}}>Example button</Text>
+      </TouchableOpacity>
+
+      {/* Example image */}
+      <Text style={{marginTop: 20}}>Example image</Text>
+      <Image
+        source = {require('./assets/images/ice_cream.png')}
+        style = {{width: 100, height: 100}}/>
+
+
+    </View>
+
+
+  );
+}
