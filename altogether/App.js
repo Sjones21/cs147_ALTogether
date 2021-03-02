b7a68229a4eedaa066d039337feb155df67e464b
Sydney Jones <47163893+Sjones21@users.noreply.github.com>
2021-03-01 21:43:53 -0600

    Update App.js

--- altogether/App.js
+++ altogether/App.js
@@ -41,7 +41,7 @@ export default function App() {
       <Image
         source = {require('./assets/images/ice_cream.png')}
         style = {{width: 100, height: 100}}/>
-        <Text>DELICIOUS! I want one. Strawberry OH YEAH. YUMMY YUMMY</Text>
+        <Text>DELICIOUS! I want one. Strawberry OH YEAH. YUMMY YUMMY YUMMY</Text>
     </View>
 
 
