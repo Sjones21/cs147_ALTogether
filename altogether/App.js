7aac5a7b28365601a36352943d6bac5919c66b3e
Sydney Jones <47163893+Sjones21@users.noreply.github.com>
2021-03-01 20:27:42 -0600

    added text to SYDNEY WAS HERE

--- altogether/App.js
+++ altogether/App.js
@@ -23,7 +23,7 @@ export default function App() {
       {/* Example text */}
       <Text>Open up App.js to start working on your app!</Text>
       <Text>If you're seeing this, it means that you set up ALTogether correctly!</Text>
-      <Text>SYDNEY WAS HERE</Text>
+      <Text>SYDNEY WAS HERE (In the new branch too)</Text>
       <StatusBar style="auto" />
 
       {/* Example button */}
