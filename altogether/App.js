83986888f3c3fd8da9dcd2b6ff4247f36443d68a
JungWonHa <52128886+JungWonHa@users.noreply.github.com>
2021-03-02 16:25:03 -1000

    card container changes

--- altogether/App.js
+++ altogether/App.js
@@ -6,7 +6,7 @@ import {
   StyleSheet
 } from "react-native";
 */
-//import { Container, Content, Icon } from 'native-base'
+import { Container, Content } from 'native-base'
 import CardComponent from './src/components/CardComponent'
 //import { Audio } from 'expo'
 import { View, Text, Image } from 'react-native'
@@ -38,8 +38,8 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <ScrollView>
+      <Container style={styles.container}>
+        <Content>
           <CardComponent thumbnail={require("./assets/me.jpg")} youtube={true} id_youtube="L-W_EDUQw6I" likes="11" nb_commentaires="202" pseudo="maxgfr" date="01/01/2018" description="blablablabla" />
           <CardComponent thumbnail={require("./assets/me.jpg")} youtube={true} id_youtube="507d9xto6Og" likes="11" nb_commentaires="202" pseudo="maxgfr" date="01/01/2018" description="blablablabla" />
           <CardComponent thumbnail={require("./assets/me.jpg")} twitch_live={true} chaine_twitch="humilityfr" likes="11" nb_commentaires="202" pseudo="maxgfr" date="01/01/2018" description="blablablabla" />
@@ -51,8 +51,8 @@ export default class App extends React.Component {
           <CardComponent thumbnail={require("./assets/me.jpg")} spotify_playlist={true} id_user="2c8a6esgmr19imm8bl14570vk" id_spotify="2280Pf3U69Rp6CdVpxzSNr" likes="11" nb_commentaires="202" pseudo="maxgfr" date="01/01/2018" description="blablablabla" />
           <CardComponent thumbnail={require("./assets/me.jpg")} spotify_song={true} id_spotify="4GXl8l1MfZPf2GvpyRPJBf" likes="11" nb_commentaires="202" pseudo="maxgfr" date="01/01/2018" description="blablablabla" />
           <CardComponent thumbnail={require("./assets/me.jpg")} pinterest={true} id_pinterest="801781539887841710" likes="11" nb_commentaires="202" pseudo="maxgfr" date="01/01/2018" description="blablablabla" />
-        </ScrollView>
-      </View>
+        </Content>
+      </Container>
     )
   }
 }
--- altogether/src/components/CardComponent.js
+++ altogether/src/components/CardComponent.js
@@ -8,10 +8,11 @@ import {
     Platform,
     Linking
 } from "react-native";
+import { styles } from '../../Styles';
 
-import { Card, CardItem, Thumbnail, Body, Left, Right, Button, Icon } from 'native-base';
+import { Container, Card, CardItem, Thumbnail, Body, Left, Right, Button, Icon } from 'native-base';
 
-import { Dimensions } from 'react-native';
+import { Dimensions, SafeAreaView, ScrollView } from 'react-native';
 
 const { width, height } = Dimensions.get('window');
 
@@ -25,27 +26,72 @@ class CardComponent extends Component {
 
     render() {
         return (
-            <Card style={styles.container}>
-                <CardItem>
-                    <Left>
-                        <Thumbnail source="./assets/me.jpg" />
-                        <Body>
-                            <Text style={{ fontWeight: "900" }}>{this.props.pseudo} </Text>
-                            <Text note>{this.props.date}</Text>
-                        </Body>
-                    </Left>
-                </CardItem>
-            </Card>
+            <SafeAreaView style={styles.container}>
+                <ScrollView style={styles.scrollView}>
+                    <Card >
+                        <CardItem>
+                            <Left>
+                                <Thumbnail style={{ height: 40, width: 40 }} source={this.props.thumbnail} />
+                                <Body>
+                                    <Text style={{ fontWeight: "900" }}>{this.props.pseudo} </Text>
+                                </Body>
+                            </Left>
+                        </CardItem>
+
+                        <CardItem >
+                            <Image source={require('../../assets/images/ice_cream.png')}
+                                style={{ height: scale(320), width: scale(320) }} />
+                        </CardItem>
+
+                        <CardItem style={{ height: 30 }}>
+                            <Left>
+                                <Button transparent>
+                                    <Icon name="ios-heart-outline" style={{ color: 'black' }} />
+                                </Button>
+                                <Button transparent>
+                                    <Icon name="ios-chatbubble-outline" style={{ color: 'black' }} />
+                                </Button>
+                                <Button transparent>
+                                    <Icon name="ios-paper-plane-outline" style={{ color: 'black' }} />
+                                </Button>
+                            </Left>
+                        </CardItem>
+                        <CardItem style={{ height: 30 }} >
+                            <Left>
+                                <Button transparent>
+                                    <Text style={{ fontWeight: "900" }}>{this.props.likes} likes </Text>
+                                </Button>
+                            </Left>
+                        </CardItem>
+                        <CardItem style={{ height: 20 }}>
+                            <Left>
+                                <Button transparent>
+                                    <Text>
+                                        <Text style={{ fontWeight: "900" }}>{this.props.pseudo} </Text>
+                                        {this.props.description}
+                                    </Text>
+                                </Button>
+                            </Left>
+                        </CardItem>
+                        <CardItem style={{ height: 30 }}>
+                            <Left>
+                                <Button transparent>
+                                    <Text style={{ color: 'gray' }}>
+                                        View all {this.props.nb_commentaires} comments
+                                </Text>
+                                </Button>
+                            </Left>
+                        </CardItem>
+                        <CardItem>
+                            <Left>
+                                <Text style={{ color: 'gray' }}>{this.props.date}</Text>
+                            </Left>
+                        </CardItem>
+
+                    </Card>
+                </ScrollView>
+            </SafeAreaView>
         );
     }
 }
 export default CardComponent;
-
-const styles = StyleSheet.create({
-    container: {
-        marginTop: 50
-    },
-    WebViewContainer: {
-    marginTop: (Platform.OS == 'android') ? 20 : 0,
-  }
-});
