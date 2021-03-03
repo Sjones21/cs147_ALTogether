896be61bbddd7e4df4cd3f99aa39acfcd1748f45
Danielle Cruz <dcruz21@stanford.edu>
2021-03-03 17:02:32 -0600

    Added header and navbar for profile page

--- altogether/App.js
+++ altogether/App.js
@@ -13,36 +13,118 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import { styles } from './Styles.js';
+// import { styles } from './Styles.js';
 
 
 export default function App() {
   return (
+
     <View style={styles.container}>
 
-      {/* Example text */}
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>If you're seeing this, it means that you set up ALTogether correctly!</Text>
-      <Text>SYDNEY WAS HERE </Text>
-      <Text>DANI WAS ALSO HERE </Text>
-      <StatusBar style="auto" />
 
-      {/* Example button */}
-      <Text style={{marginTop: 20}}>Example button</Text>
-      <TouchableOpacity
-        style={styles.exampleButton}
-        onPress={() => Alert.alert('You just pressed an example button')}>
-        <Text style={{color: 'white', fontWeight: 'bold'}}>ICE CREAM</Text>
-      </TouchableOpacity>
+      {/* Header bar */}
+      <View style={styles.header}>
+        <View>
+          <Text style={styles.headerTitle}>sydney_jones</Text>
+        </View>
+        <View style={styles.barIcons}>
+          <Image
+            source = {require('./assets/images/Post.png')}
+            style = {styles.icons}/>
+          <Image
+            source = {require('./assets/images/Menu.png')}
+            style = {styles.icons}/>
+        </View>
+      </View>
 
-      {/* Example image */}
-      <Text style={{marginTop: 20}}>Example image</Text>
-      <Image
-        source = {require('./assets/images/ice_cream.png')}
-        style = {{width: 100, height: 100}}/>
-        <Text>DELICIOUS!</Text>
+      {/* Scroll */}
+      <ScrollView
+        style={styles.scrollContainer}>
+        {/* Profile */}
+        <View>
+          <Text>sydney_jones</Text>
+        </View>
+
+        {/* Photos */}
+        <View>
+        </View>
+      </ScrollView>
+
+      {/* Navigation bar */}
+      <View style={styles.navBar}>
+        <Image
+          source = {require('./assets/images/Home.png')}
+          style = {styles.icons}/>
+        <Image
+          source = {require('./assets/images/Search.png')}
+          style = {styles.icons}/>
+        <Image
+          source = {require('./assets/images/LogoSmall.png')}
+          style = {styles.icons}/>
+        <Image
+          source = {require('./assets/images/Bookmark.png')}
+          style = {styles.icons}/>
+        <Image
+          source = {require('./assets/images/ProfileIcon.png')}
+          style = {styles.icons}/>
+      </View>
+
     </View>
 
 
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 20,
+    backgroundColor: '#fff',
+  },
+
+  exampleButton: {
+    padding: 10,
+    backgroundColor: '#3996EF',
+    borderRadius: 10,
+  },
+
+  header: {
+    height: 90,
+    padding: 30,
+    paddingBottom: 10,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'space-between',
+    backgroundColor: '#FAFAFA'
+  },
+
+  headerTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+
+  barIcons: {
+    flexDirection: 'row'
+  },
+
+  icons: {
+    height: 25,
+    width: 25,
+    margin: 5
+  },
+
+  scrollContainer: {
+  flexGrow: 1,
+  margin: 20,
+},
+
+  navBar: {
+    height: 100,
+    padding: 30,
+    paddingTop: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    backgroundColor: '#FAFAFA'
+  }
+});
