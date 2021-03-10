2be51177a7c72ebffaa861eca00e221284aac9b6
JungWonHa <52128886+JungWonHa@users.noreply.github.com>
2021-03-10 11:08:38 -1000

    changed JPG to jpg

--- altogether/NewPostChoosePic.js
+++ altogether/NewPostChoosePic.js
@@ -5,10 +5,18 @@ import { Container, Content } from 'native-base'
 import CardComponent from './src/components/CardComponent'
 //import { Audio } from ''
 import { Card, ListItem, Button, Icon } from 'react-native-elements'
+import { GALLERY } from './GALLERY.js';
 import { styles } from './Styles.js';
 
 export default class NewPostChoosePic extends Component {
   render() {
+    let images = [];
+
+    for (const [key, value] of Object.entries(GALLERY)) {
+      images.push(
+        <GalleryPhoto key={`${key}`} photo={value} />
+      );
+    }
     return (
       <View style={styles.container}>
         <View style={styles.titleContainer}>
